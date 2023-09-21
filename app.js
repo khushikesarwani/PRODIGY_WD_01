5b504f67d837e428b383d698baa89c754299f9a2
Khushi <khushikeserwani3830@gmail.com>
2023-09-21 11:57:35 +0530

    saved all files

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+window.addEventListener('scroll',()=>{
+  const scrolled=window.scrollY;
+ 
+  if(scrolled>486)
+  {
+  document.querySelector('.container').classList.add('gradient-background');
+  
 
+  }
+  if(scrolled<486){
+      document.querySelector('.container').classList.remove('gradient-background');
+     
+  }
+});
 
-
+// for hamburger toggler
 var toggler=document.querySelector('.mytoggle');
 toggler.addEventListener("click",handleClick);
 function handleClick(){
@@ -9,10 +22,27 @@ function handleClick(){
       
 }
 
-
+// for making the form show on clicking the register button
 var formdisp= document.querySelector('.register');
 formdisp.addEventListener("click",handleform);
 function handleform(){
   document.querySelector('.box').classList.toggle('showform');
   document.querySelector('.main').classList.toggle('hidecontent');
+}
+
+window.addEventListener('scroll',reveal2);
+function reveal2(){
+    var reveals=document.querySelectorAll('.reveal2');
+    for(var i=0;i<reveals.length;i++)
+    {
+    var windowheight=window.innerHeight;
+    var revealtop=reveals[i].getBoundingClientRect().top;
+    var revealpoint=150;
+    if(revealtop<windowheight-revealpoint){
+        reveals[i].classList.add('reveal2-active');
+    }
+    else{
+        reveals[i].classList.remove('reveal2-active');
+    }
+}
 }
